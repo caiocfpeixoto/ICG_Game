ad7c198ffb738b2beca20a864b070f284f5a90b4
Luciano Ramos <thalmenoch@gmail.com>
2023-11-01 19:46:35 -0300

    pulo na parede implementado

--- ICG - Game/Assets/Scripts/PlayerMovement.cs
+++ ICG - Game/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D body;
     private Animator anim;
     private BoxCollider2D boxCollider;
+    private float wallJumpCooldown;
 
     private void Awake()
     {
@@ -19,19 +20,37 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2( horizontalInput * speed, body.velocity.y);
 
         // FLIP DO PLAYER QUANDO MOVER ESQ/DIR
         if(horizontalInput > 0.01f)
-            transform.localScale = new Vector3(2, 2, 2);
+            transform.localScale = new Vector3((float)2.5, (float)2.5, (float)2.5);
         else if(horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-2, 2, 2);
+            transform.localScale = new Vector3((float)-2.5, (float)2.5, (float)2.5);
 
-        if(Input.GetKey(KeyCode.Space) && isGrounded())
-            Jump();
-
+        //parêmetros de animação
         anim.SetBool("run", horizontalInput != 0);
         anim.SetBool("grounded", isGrounded());
+
+        // print(onWall());
+        // Lógica do pulo na parede
+        if (wallJumpCooldown < 0.2f)
+        {
+            if(Input.GetKey(KeyCode.Space) && isGrounded())
+                Jump();
+
+            body.velocity = new Vector2( horizontalInput * speed, body.velocity.y);
+
+            if (onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+            else 
+                body.gravityScale = 2;
+        }
+        else
+            wallJumpCooldown += Time.deltaTime; 
+
     }
 
     private void Jump()
