76d84029e16662823afba1b5f28cad36a892b0cd
Luciano Ramos <thalmenoch@gmail.com>
2023-12-11 20:47:40 -0300

    parallax parcial

--- ICG - Game/Assets/Scripts/BackGround/BackGroundScroller.cs
+++ ICG - Game/Assets/Scripts/BackGround/BackGroundScroller.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class BackGroundScroller : MonoBehaviour
+public class EfeitoParallax : MonoBehaviour
 {
-    
-    
+    [SerializeField] private Image fundo;
+    [SerializeField] private float velocidade;
+
+    private void Update()
+    {
+        MoveFundo();
+    }
+
+    public void MoveFundo()
+    {
+        transform.position = new Vector3(transform.position.x - velocidade * Time.deltaTime * Input.GetAxisRaw("Horizontal"), 0, 0);
+
+        if(transform.localPosition.x >= fundo.preferredWidth)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x - fundo.preferredWidth * 2, 0, 0);            
+        } else if(transform.localPosition.x <= -fundo.preferredWidth)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x + fundo.preferredWidth * 2, 0, 0);
+        }
+    }
 }
